cab4dcdfb3fee3b51445648503e46c08b216c561
FriptuT <friptut@gmail.com>
2021-08-06 15:38:21 +0300

    setting up the styling

--- my-portofolio/src/app/app.component.ts
+++ my-portofolio/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+ signupForm: FormGroup;
+
+ ngOnInit() {
+   this.signupForm = new FormGroup({
+     'username': new FormControl(null, Validators.required),
+     'email': new FormControl(null, [Validators.required, Validators.email]),
+     'textzone': new FormControl(null, Validators.required)
+   });
+ }
+
+ onSubmit(){
+   console.log(this.signupForm.value);
+   this.signupForm.reset();
+ }
 }
